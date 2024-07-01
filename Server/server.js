637610112334cf7a345164a2b6263ea281261285
Anjee <94264810+Th0mas00@users.noreply.github.com>
2024-07-01 20:32:27 +0800

    Backed

---
+++ Server/server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const cors = require('cors');
+const rosRest = require('ros-rest');
+require('dotenv').config();
+const app = express();
+app.use(cors());
+
+
+const clientRosRest = rosRest({
+   host: process.env.REACT_APP_BASE_URL,
+   user: process.env.REACT_APP_USERNAME,
+   password: process.env.REACT_APP_PASSWORD,
+   port: 443, // default 443
+   secure: false, // default false
+});
+
+clientRosRest
+   .print('system/resource')
+   .then((res) => {
+      console.log('result:', res.data);
+   })
+   .catch((err) => {
+      console.log('error:', err);
+   });
+
+app.get("/api", (req, res) => {
+   res.json({ "users": ["Sarip", "userTwo", "userThree"] })
+})
+
+
+app.listen(5000, () => {
+   console.log("Server started on port 5000")
+})
