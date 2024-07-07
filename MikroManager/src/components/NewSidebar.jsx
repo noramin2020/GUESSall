110c4f39c8a992beb65d5697418e99197ade6481
Mohammad Mujib Tocalo <mujibtocalo@yahoo.com>
2024-07-07 21:13:52 +0800

    sidebar height adjustment

--- MikroManager/src/components/NewSidebar.jsx
+++ MikroManager/src/components/NewSidebar.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 const NewSidebar = () => {
 	return (
-		<Sidebar>
+		<Sidebar className='h-screen'>
 			<Menu menuItemStyles={{
 				button: {
 					// the active class will be added automatically by react router
