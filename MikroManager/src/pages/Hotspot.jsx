e6f7db6688e206d24ea226adda354ec362ca43f2
Mohammad Mujib Tocalo <mujibtocalo@yahoo.com>
2024-07-08 14:42:01 +0800

    deleted hotspot page

--- MikroManager/src/pages/Hotspot.jsx
+++ MikroManager/src/pages/Hotspot.jsx
@@ -1,9 +1,36 @@
+import DataTable from "react-data-table-component"
+const columns = [
+	{
+		name: 'Title',
+		selector: row => row.title,
+	},
+	{
+		name: 'Year',
+		selector: row => row.year,
+	},
+];
 
+const data = [
+	{
+		id: 1,
+		title: 'Beetlejuice',
+		year: '1988',
+	},
+	{
+		id: 2,
+		title: 'Ghostbusters',
+		year: '1984',
+	},
+]
 
 export function UserList() {
 	return (
-		<div>
-			Userlist
+		<div className="p-2">
+			<h1 className="font-semibold" >User List</h1>
+			<DataTable
+				columns={columns}
+				data={data}
+			/>
 		</div>
 	)
 }
@@ -11,7 +38,8 @@ export function UserList() {
 export function AddUser() {
 	return (
 		<div>
-			AddUser
+			<h1 className="font-semibold" >Add user</h1>
 		</div>
 	)
 }
+
