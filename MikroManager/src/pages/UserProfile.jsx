3f7387d52e77e9eb8626d76af24352822dc89485
Mohammad Mujib Tocalo <mujibtocalo@yahoo.com>
2024-07-24 21:32:22 +0800

    Add Function options

--- MikroManager/src/pages/UserProfile.jsx
+++ MikroManager/src/pages/UserProfile.jsx
@@ -26,31 +26,33 @@ const data = [
 export function ProfileList() {
 	return (
 		<div className="w-full p-10">
-		<h1 className="pl-2 bg-customBlue p-2 rounded-md text-white font-bold" >Profile List</h1>
-		<DataTable
-			columns={columns}
-			data={data}
-			className="border mt-2 rounded-md"
-		/>
-	</div>
+			<h1 className="pl-2 bg-customBlue p-2 rounded-md text-white font-bold" >Profile List</h1>
+			<DataTable
+				columns={columns}
+				data={data}
+				className="border mt-2 rounded-md"
+			/>
+		</div>
 	)
 }
 
 export function AddProfile() {
 	return (
-		<div>
-
+		<div className="w-2/4 p-10">
 			<form className="flex flex-col border m-2 p-2">
-				<h1 className="font-semibold bg-customBlue text-white text-center p-2" >Add Profile</h1>
-				<div className="flex m-2 p-2">
-					<h2 className="p-1">Title</h2>
-					<input className="border" type="text" />
+				<h1 className="font-semibold bg-customBlue text-white text-center p-2" >Add user</h1>
+				<div className="w-1/2">
+					<div className="flex m-2 p-2">
+						<h2 className="p-1">Name</h2>
+						<input className="border bg-transparent rounded-md" type="text" />
+					</div>
+					<div className="flex m-2 px-2">
+						<h2 className="p-1 whitespace-pre">Rate Limit (rx/tx)</h2>
+						<input className="border bg-transparent rounded-md" type="text" />
+					</div>
+
 				</div>
-				<div className="flex m-2 p-2">
-					<h2 className="p-1">Year</h2>
-					<input className="border" type="text" />
-				</div>
-				<button className="flex border mx-10 p-1 justify-center">Submit</button>
+				<button className="flex border w-36 mx-10 p-1 mt-2 justify-center rounded-md bg-customBlue text-white self-center">Submit</button>
 			</form>
 		</div>
 	)
