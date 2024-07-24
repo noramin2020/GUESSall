bddd66367b10aa84062e99a3e6f6ec9c750093d0
Anjee <94264810+Th0mas00@users.noreply.github.com>
2024-07-24 21:19:34 +0800

    styled hotspot

--- MikroManager/src/pages/Hotspot.jsx
+++ MikroManager/src/pages/Hotspot.jsx
@@ -44,19 +44,24 @@ export function UserList() {
 
 export function AddUser() {
 	return (
-		<div>
-
+		<div className="w-2/4 p-10">
 			<form className="flex flex-col border m-2 p-2">
 				<h1 className="font-semibold bg-customBlue text-white text-center p-2" >Add user</h1>
-				<div className="flex m-2 p-2">
-					<h2 className="p-1">Title</h2>
-					<input className="border" type="text" />
+				<div className="w-1/2">
+					<div className="flex m-2 p-2">
+						<h2 className="p-1">Name</h2>
+						<input className="border bg-transparent rounded-md" type="text" />
+					</div>
+					<div className="flex m-2 px-2">
+						<h2 className="p-1">Profile</h2>
+						<input className="border bg-transparent rounded-md" type="text" />
+					</div>
+					<div className="flex m-2 px-2">
+						<h2 className="p-1">Uptime</h2>
+						<input className="border bg-transparent rounded-md" type="text" />
+					</div>
 				</div>
-				<div className="flex m-2 px-2">
-					<h2 className="p-1">Year</h2>
-					<input className="border" type="text" />
-				</div>
-				<button className="flex border mx-10 p-1 mt-2 justify-center">Submit</button>
+				<button className="flex border w-36 mx-10 p-1 mt-2 justify-center rounded-md bg-customBlue text-white self-center">Submit</button>
 			</form>
 		</div>
 	)
